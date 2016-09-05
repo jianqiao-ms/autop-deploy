0d3e25cb3bb9db758fe90faf6564dafc29ee8800
jianqiao <jianqiao.ms@foxmail.com>
2016-09-05 18:13:19 +0800

    `

--- autop-deploy.py
+++ autop-deploy.py
@@ -9,13 +9,16 @@ import datetime
 import subprocess
 
 import tornado.ioloop
+import tornado.options
 from tornado.web import RequestHandler
 from tornado.web import Application
-import tornado.options
+from tornado.web import asynchronous
 from tornado.gen import coroutine
 
 from tasks import mysql_query
 from tasks import mysql_get
+from tasks import deploy
+from tasks import sleep
 
 # 自定义方法，格式化返回数据
 class DateJsonEncoder(json.JSONEncoder, object):
@@ -51,76 +54,22 @@ settings = dict(
 def make_app():
     return Application([
         (r"/", Index),
-        (r"^/assets", Assets),
-        (r"^/assets/([a-zA-Z]+)", Assets),
-        (r"^/assets/([a-zA-Z]+)/([a-zA-Z]+)", Assets),
-        (r"^/deploy", Deploy),
-        (r"^/deploy/([a-zA-Z]+)", Deploy)
+        # (r"^/deploy", Deploy),
+        (r"^/deploy/([0-9]+)", Deploy)
     ], **settings)
 
 class Index(RequestHandler, object):
     def get(self):
         self.render("base.html")
 
-class Assets(RequestHandler, object):
-    @coroutine
-    def get(self, item ='', functions = ''):
-        item_table = dict(host      = "t_assets_host",
-                          hosttype  = "t_assets_hosttype",
-                          hostgroup = "t_assets_hostgroup",
-                          env       = "t_assets_env",
-                          project   = "t_assets_project")
-
-        item_pages = dict(host      = "assets-host.html",
-                          hosttype  = "assets-hosttype.html",
-                          hostgroup = "assets-hostgroup.html",
-                          env       = "assets-env.html",
-                          project   = "assets-project.html")
-
-        if not functions:
-            if not item:
-                self.render("assets.html",result = [])
-            else:
-                result = mysql_query("SELECT * FROM {table_name}".format(table_name=item_table[item]))
-
-                envs = None
-                if item == 'project':
-                    envs = mysql_query("SELECT * FROM t_assets_env")
-                self.render(item_pages[item], result = result, envs = envs)
-        else:
-            if functions == 'settings' and item == 'project':
-                pname_path          = dict(imanager_core        = 'imanager',
-                                           imanager_web         = 'imanager_web',
-                                           imanager_api         = 'imanager_api',
-                                           imanager_iservice    = 'imanager_iservice',
-                                           iservice             = 'iservice')
-                id                  = self.get_argument('id')
-                env_id              = self.get_argument('env_id')
-
-                project             = mysql_get("SELECT * FROM t_assets_project WHERE id='{id}'".format(id=id))
-                envs                = mysql_get("SELECT * FROM t_assets_env WHERE id={id}".format(id=env_id))
-                project_setting     = mysql_get("SELECT * FROM t_assets_project_deploy_settings WHERE "
-                                                "project_id={pid} AND env_id={eid}".format(pid=id, eid=env_id))
-                hosts               = mysql_query("SELECT * FROM t_assets_host WHERE env_id={eid}".format(eid=env_id))
-                hostgroups          = mysql_query("SELECT * FROM t_assets_hostgroup WHERE env_id={eid}".format(eid=env_id))
-                branches            = subprocess.check_output("cd /var/run/autop/%s;git branch -a|grep remotes|grep -v HEAD|awk -F '/' '{print $3}'"
-                                                              %pname_path[project[0].name],
-                                                              shell=True)
-                self.render("assets-project-settings.html", project = project, envs = envs, settings = project_setting ,
-                            hosts = hosts, hostgroups = hostgroups, branches = branches.split('\n')[:-1])
 
 class Deploy(RequestHandler, object):
-    def get(self, item = '', functions = ''):
-        item_table = dict(running="t_deploy_running",
-                          history="t_deploy_history")
-
-        if not item:
-            self.render("deploy.html", result='')
-        elif item == 'new':
-            self.render("deploy_new.html")
-        else:
-            result = mysql_query("SELECT * FROM {table_name}".format(table_name=item_table[item]))
-            self.render("deploy.html", result=result)
+    @asynchronous
+    @coroutine
+    def get(self, pid = ''):
+        self.write(time.strftime("%Y-%d-%m %H:%M:%S"))
+        self.write(time.strftime("%Y-%d-%m %H:%M:%S"))
+        self.finish('Done')
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
--- tasks.py
+++ tasks.py
@@ -4,6 +4,9 @@
 import torndb
 from celery import Celery
 
+import subprocess
+import time
+
 celery = Celery("task")
 celery.config_from_object('celeryconf')
 
@@ -12,6 +15,28 @@ db = torndb.Connection(host="192.168.0.195",database="autop",user='cupid',passwo
 @celery.task
 def mysql_query(cmd):
     return db.query(cmd)
-
+@celery.task
 def mysql_get(cmd):
     return db.query(cmd)
+@celery.task
+def deploy(pname):
+    name_path = {'imanager_bi'      : 'imanager_bi',
+                 'imanager_web'     : 'imanager_web',
+                 'imanager_api'     : 'imanager_api',
+                 'imanager_core'    : 'imanager',
+                 'imanager_iclock'  : 'imanager_iclock',
+                 'imanager_iservice': 'iservice',
+                 'iservice'         : 'iservice',
+                 'tf-oa'            : 'tf-oa',
+                 'ApiForApp'        : 'ApiForApp'
+                 }
+
+    # 切换目录
+    cmd = "cd /var/run/autop/repo/{path}".format(path = name_path[pname])
+    a = subprocess.check_output(cmd,shell=True)
+    return a
+
+@celery.task
+def sleep(n):
+    time.sleep(5)
+    return 0
