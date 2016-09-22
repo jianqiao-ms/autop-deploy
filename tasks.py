184ca82b8f06d7ef75e4e2cd65b43e248e2945f2
jianqiao <jianqiao.ms@foxmail.com>
2016-09-22 11:58:04 +0800

    1

--- tasks.py
+++ tasks.py
@@ -117,7 +117,8 @@ def deploy(pname):
             break
     if compile_flag:
         try:
-            subprocess.check_output("mvn clean install", shell=True)
+            a = subprocess.check_output("echo $PATH", shell=True)
+            print a
             data['msg'].append('[OK]编译成功')
         except subprocess.CalledProcessError, e:
             data['msg'].append('[ERROR]编译失败')
