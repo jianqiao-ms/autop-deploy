36b3a95c9c4325ec255cc5f9073eec6cf2e020d3
jianqiao <jianqiao.ms@foxmail.com>
2016-09-27 12:05:26 +0800

    完成offcanvas

--- static/js/js-admin.js
+++ static/js/js-admin.js
@@ -4,21 +4,31 @@
 
 $(document).ready(function () {
     var offcanvas               = $("div.ams-container-offcanvas");
-    var offcancas_toggle        = $("div.ams-rectangle-right");
-    var offcancas_toggle_line   = $("div.ams-rectangle-right-in");
+    var offcancasToggle        = $("div.ams-rectangle-right");
+    var offcancasToggleLine   = $("div.ams-rectangle-right-in");
 
+    $(window).resize(function () {
+        if ($(window).width() > 640) {
+            offcanvas.css({left:0})
+        }
+        else {
+            offcanvas.css({left:'-270px'})
+        }
+    });
 
-    offcancas_toggle.click(function () {
+    offcancasToggle.click(function () {
         var offcancasStatus = offcanvas.css('left');
         if (offcancasStatus == '-270px'){
             offcanvas.animate({left:'0px'});
-            offcancas_toggle.animate({opacity:'1'});
-            offcancas_toggle_line.css({left:'2px'});
+            offcancasToggle.animate({opacity:'1'});
+            offcancasToggleLine.animate({left:'2px'});
         }
         if (offcancasStatus == '0px'){
             offcanvas.animate({left:'-270px'});
-            offcancas_toggle.animate({opacity:'0.3'});
-            offcancas_toggle_line.css({left:'6px'});
+            offcancasToggle.animate({opacity:'0.3'});
+            offcancasToggleLine.animate({left:'6px'});
         }
     });
+
+
 });
