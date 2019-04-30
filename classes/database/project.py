d0bcb082df85b9edd24cd57f630b43f7c1b5ff25
jianqiao <jianqiao.ms@gmail.com>
2019-04-30 18:21:56 +0800

    before 51

--- classes/database/project.py
+++ classes/database/project.py
@@ -16,16 +16,26 @@ from public import BaseTable, Base
 # Class&Function Defination
 class TableProject(BaseTable):
     """自关联表"""
-    __tablename__ = 'project'
-    id = Column(Integer, primary_key=True)
-    gitlab_id = Column(Integer)
-    role = Column(Enum('repo', 'project', 'public'), nullable = True)
+    __tablename__   = 'project'
+
+    id              = Column(Integer, primary_key=True)
+    gitlab_id       = Column(Integer)
+    name            = Column(String(32))
+    build_cmd       = Column(String(128))
+    role            = Column(Enum('repo', 'project', 'public'), nullable = True)
+    path            = Column(String(32))
+    artifact_path   = Column(String(128))
+
     repo_id = Column(Integer, ForeignKey('project.id'))
-    path = Column(String(32))
-    artifact_path = Column(String(128))
-    children = relationship('TableProject',
-                backref = backref('parent', remote_side = [id])
-            )
+    children        = relationship('TableProject', backref = backref('parent', remote_side = [id]))
+
+class TableProjectCIHistory(BaseTable):
+    __tablename__ = "project_ci_history"
+
+    id = Column(Integer, primary_key=True)
+    project_id = Column(Integer, ForeignKey('project.id'))
+    branch = Column(String(32))
+    commit = Column(String(8))
 
 
 # Logic
@@ -44,4 +54,5 @@ if __name__ == '__main__':
     ))
     # Session = sessionmaker(bind=engine)
     # session = Session()
+    Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(engine)
