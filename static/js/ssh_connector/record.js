2539cb2eab92b9f69b247821fbf7b409fef6ce70
jianqiao.ms <jianqiao.ms@gmail.com>
2019-05-30 21:37:15 +0800

    record movied worked!!!!!!

--- static/js/ssh_connector/record.js
+++ static/js/ssh_connector/record.js
@@ -3,29 +3,61 @@ function new_terminal() {
     let ws_url      = protocol+'://'+window.location.host+ '/recordwebsocket';
     let ws          = new WebSocket(ws_url);
     let terminal    = new Terminal();
-    let records     = {};
-    
+    // let records     = {};
     
     ws.onopen = function (e) {
-        ws.onmessage = function (e) {
-            records.data = e.data;
+        terminal.fit();
+        ws.onmessage = function (ee) {
+            let records_line = ee.data;
+            let records = records_line.split('endtimestamp\n');
+            // console.log(records);
+            let startTimestamp = parseInt(records[0].substring(11, 22));
+
+            // for (let i=0; i < records.length; i++) {
+            //     let newTimestamp = parseInt(records[i].substring(11, 22));
+            //     let timeDelt = newTimestamp - startTimestamp;
+            //     console.log(timeDelt)
+                // window.setTimeout(console.log(timeDelt), timeDelt);
+                // setTimeout(terminal.write(records[i].substring(22)), timeDelt);
+                // setTimeout(function () {
+                //     terminal.write('log')
+                // } , timeDelt);
+            // }
+
+            // for (var i = 0; i < 5; i++) {
+            //     setTimeout(function (){
+            //         console.log(i);
+            //         },1000);
+            // }
+
+            for (let i=0; i < records.length; i++) {
+                let newTimestamp = parseInt(records[i].substring(11, 22));
+                let timeDelt = newTimestamp - startTimestamp;
+                (function (__timeDelt) {
+                    setTimeout(function (){
+                        terminal.write(records[i].substring(22))
+                    }, __timeDelt);
+                })(timeDelt)
+            }
+
         };
-        ws.close();
-        
     };
 
-    
     window.onresize = function() { 
         terminal.fit();
     };
-    
     terminal.open(document.getElementById('terminal-container'));
-    return {records: records, terminal: terminal};
 }
 
 $(document).ready(function() {
     Terminal.applyAddon(fit);
-    term = new_terminal();
-    term.terminal.fit();
-    term.terminal.write('456');
+    new_terminal();
+
+    // for (var i = 0; i < 5; i++) {
+    //     (function(ii){   //立刻执行函数
+    //         setTimeout(function (){
+    //             console.log(ii);
+    //         },ii * 1000);
+    //     })(i);
+    // }
 });
